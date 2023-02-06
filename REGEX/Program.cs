658824671e6dd1dda02e27fb33750a4af0cc958d
Sefa Dudu <sefa.dudu@rsu.com.tr>
2023-02-06 13:37:44 +0300

    Add project files.

---
+++ REGEX/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+using System.Text.RegularExpressions;
+
+var desc = "<h3>Genel Bilgi</h3>";
+string pattern = "<[^>]+>";
+
